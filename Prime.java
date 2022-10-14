df01b6f4797ed9d4534364c8b4f2196a105e3d0a
HERVE CLAUDEL INEZA <99665264+InezaHCl@users.noreply.github.com>
2022-10-14 22:37:55 +0530

    Working_1

---
+++ Prime.java
@@ -0,0 +1,26 @@
+/*The challenge given to us today is to check if any given input number is prime or not.*/
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter a positve number: "); //Asking user to enter a number
+        int num = sc.nextInt(); //Taking input from user
+
+        boolean isPrime = true;
+
+        for(int i=2; i<num; i++){
+            if(num % 2 == 0){
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime)
+        System.out.println(num+" is Prime number");
+
+        else
+            System.out.println(num+" is not Prime number");
+    }
+}
